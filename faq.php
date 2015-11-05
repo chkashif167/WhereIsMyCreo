3f27e4332f998ac17831f23398d4b5b607af1af8
nzsiddiqui <nzsiddiqui@gmail.com>
2015-11-05 17:07:21 +0500

    adding html structure basic file structure

---
+++ faq.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include 'includes/header-top.php'; ?>
+<title>Where Is My Creo | FAQ</title>
+</head>
+<body>
+<header id="header" class="header">
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/banner2.php'; ?>
+</header><!-- /header -->
+
+<div class="container">
+<?php include 'includes/faq-content.php'; ?>
+</div>
+
+<footer class="site-footer">
+<?php include 'includes/footer.php'; ?>
+</footer>
+</body>
+</html>
---
+++ includes/header-top.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/fontsmoothie.min.js"></script>
