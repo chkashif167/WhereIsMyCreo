9b25325a86ef1231ca44acca66600fdbc7fce782
nzsiddiqui <nzsiddiqui@gmail.com>
2015-11-07 14:01:03 +0500

    my cart template

--- cart.php
+++ cart.php
@@ -1,9 +1,29 @@
-<?php 
-$giftfor = $_POST["giftmine"];
-$giftreceiver = $_POST["giftreceiver"];
-$email = $_POST["email"];
-$message = $_POST["message"];
+<!DOCTYPE html>
+<html>
+<?php
+// $giftfor = $_POST["giftmine"];
+// $giftreceiver = $_POST["giftreceiver"];
+// $email = $_POST["email"];
+// $message = $_POST["message"];
 
-echo $giftfor."<br/>";
-echo $message;
+// echo $giftfor."<br/>";
+// echo $message;
 ?>
+<head>
+<?php include 'includes/header-top.php'; ?>
+<title>Where Is My Creo | Cart</title>
+</head>
+<body>
+<header id="header" class="header">
+<?php include 'includes/header.php'; ?>
+</header><!-- /header -->
+
+<div class="container">
+
+</div>
+
+<footer class="site-footer">
+<?php include 'includes/footer.php'; ?>
+</footer>
+</body>
+</html>
