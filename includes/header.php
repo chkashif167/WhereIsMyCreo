48555862dd9bbb6b2a46d7866d4caa37f844e9ab
nzsiddiqui <nzsiddiqui@gmail.com>
2015-11-09 18:05:46 +0500

    header remaining styling

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,17 @@
 <div class="container">
 <div class="logo col-sm-2"><a href="index.php"><img src="images/logo.png"></a></div>
-<div class="col-sm-10 paddingzeroR">
+<div class="col-sm-10 paddingzero">
 	<ul class="main-menu">
 	<li><a href="buytshirts.php">Buy T-Shirts</a></li>
 	<li><a href="#">How Does This Work</a></li>
 	<li><a href="faq.php">Faq/Contact</a></li>
-	<li class="freetshirt"><a>Get Free T-Shirt</a></li>
+	<li class="freetshirt"><a>Get Free T-Shirt</a>
+	<div class="freeshirt-container">
+		<h4>365 Days of Awesomeness.</h4>
+		<p>Hop on the freebie bandwagon. Sign up for 1 year and we will sneak in an extra t-shirt of our choice with your first delivery.   Woot Woot!</p>
+		<a href="#" class="btn-subscribe">Subscribe Now</a>
+	</div>
+	</li>
 	<li class="mini-cart"><a href="cart.php">My Cart <span>1</span></a></li>
 	</ul>
 </div>
