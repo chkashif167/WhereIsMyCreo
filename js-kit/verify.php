c88bcc347a5e2c97fc402d297f0a6feceb3367a8
nzsiddiqui <nzsiddiqui@gmail.com>
2015-11-12 19:46:20 +0500

    Email template changes

--- js-kit/verify.php
+++ js-kit/verify.php
@@ -21,28 +21,28 @@ $charge = $apiClient->chargeService();
       </script>
       <?php
       else:
-	    $mine = $_POST["mine"]; 
-	    if($mine == "mine"):
-	    	$mine = "for Yourself";
-	    elseif($mine == "mine"):
-	    	$mine = "as a GIFT for someone";
-	    endif;
-	    $size = $_POST["size"];
-	    $gender = $_POST["gender"];
-	    $shirttype = $_POST["shirttypes"];
-	    $favcolor = $_POST["favcolor"];
-	    $favmovie = $_POST["favmovie"];
-	    $favsinger = $_POST["favsinger"];
-	    //$totalprice = $_POST["totalprice"];
-	    $subscriptions = $_POST["subscription"];
-	    $country = $_POST["country"];
-	    $city = $_POST["city"];
-	    $mobile = $_POST["mobile"];
-	    $email = $_POST["email"];
-	    $address = $_POST["address"];
-	    $totalprice = $_POST["totalprice"];
-	    $totalpricenew = $_POST["totalpricenew"];
-	    $creditcard = $_POST["creditcard"];
+      $mine = $_POST["mine"]; 
+      if($mine == "mine"):
+        $mine = "for Yourself";
+      elseif($mine == "mine"):
+        $mine = "as a GIFT for someone";
+      endif;
+      $size = $_POST["size"];
+      $gender = $_POST["gender"];
+      $shirttype = $_POST["shirttypes"];
+      $favcolor = $_POST["favcolor"];
+      $favmovie = $_POST["favmovie"];
+      $favsinger = $_POST["favsinger"];
+      //$totalprice = $_POST["totalprice"];
+      $subscriptions = $_POST["subscription"];
+      $country = $_POST["country"];
+      $city = $_POST["city"];
+      $mobile = $_POST["mobile"];
+      $email = $_POST["email"];
+      $address = $_POST["address"];
+      $totalprice = $_POST["totalprice"];
+      $totalpricenew = $_POST["totalpricenew"];
+      $creditcard = $_POST["creditcard"];
       $to=$email;
       $subject = "creo Subscription";
 
@@ -54,21 +54,17 @@ $charge = $apiClient->chargeService();
       $header .= "Content-type: text/html; charset: utf8\r\n";
       $headers .= 'From: info@creoroom.com' . "\r\n"; 
 
-      $message = "<p>You have successfully subscribed to <b>".$subscriptions."</b></p>
-      <h3>Detail:</h3><p>You have selected shirt for <b>".$mine."</b>Which is a <b>".$size."</b> shirt with <b>".$shirttype."</b> and color is <b>".$favcolor."</b><br />
-      <b>Favourite Movie:</b> ".$favmovie."<br /><b>Favourite Singer</b> ".$favsinger."</p><br /><p>It will be shiped to you on address<br />
-      <b>Country:</b> ".$country."<br /><b>City:</b> ".$city."<br /><b>Address:<b> ".$address."</p><p>This amount has been detucted from your account ".$totalpricenew." Your total bill is <b>".$totalprice."</b>. Thanks for Purchasing</p>";
-  
+      $message = "<p>You have successfully subscribed to <b><?php echo $subscriptions; ?></b></p><h3>Detail:</h3><p>You have selected shirt for <b><?php echo $mine; ?></b>Which is a <b><?php echo $size; ?></b> shirt with <b><?php echo $shirttype; ?></b> and color is <b><?php echo $favcolor; ?></b><br /><b>Favourite Movie:</b> <?php echo $favmovie; ?><br /><b>Favourite Singer</b> <?php echo $favsinger; ?></p><br /><p>It will be shiped to you on address<br /><b>Country:</b> <?php echo $country; ?><br /><b>City:</b> <?php echo $city; ?><br /><b>Address:<b> <?php echo $address; ?></p><p>This amount has been detucted from your account <?php echo $totalpricenew; ?>Your total bill is <b><?php echo $totalprice; ?></b>. Thanks for Purchasing</p>";
+
       mail($to,$subject,$message,$headers);
 
-      $messagenew="<p>Customer have succesfully subscribed <b>".$subscriptions."</b></p>
-      <h3>Detail:</h3><p>You have selected shirt <b>".$mine."</b> Which is a <b>".$size."</b> shirt with <b>".$shirttype."</b> and color is <b>".$favcolor."</b><br /><b>Favourite Movie:</b> ".$favmovie."<br /><b>Favourite Singer:</b> ".$favsinger."</p><p>It will be shiped to this address<br />
-      <b>Country:</b> ".$country."<br /><b>City:</b> ".$city."<br /><b>Address:</b> ".$address."</p><p>This amount has been detucted from his account ".$totalpricenew." His total bill is <b>".$totalprice."</b> Thanks for Purchasing</p>";
+      $messagenew="<p>Customer have succesfully subscribed <b><?php echo $subscriptions; ?></b></p><h3>Detail:</h3><p>You have selected shirt <b><?php echo $mine; ?></b> Which is a <b><?php echo $size; ?></b> shirt with <b><?php echo $shirttype; ?></b> and color is <b><?php echo $favcolor; ?></b><br /><b>Favourite Movie:</b> <?php echo $favmovie; ?><br /><b>Favourite Singer:</b><?php echo $favsinger; ?></p><p>It will be shiped to this address<br /><b>Country:</b> <?php echo $country; ?><br /><b>City:</b><?php echo $city; ?><br /><b>Address:</b> <?php echo $address; ?></p><p>This amount has been detucted from his account <?php echo $totalpricenew; ?> His total bill is <b><?php echo $totalprice; ?></b> Thanks for Purchasing</p>";
+
       mail("humaira.batool@progos.org","customer Subscription",$messagenew,$headers);
       if(mail($to,$subject,$message,$headers) && mail("humaira.batool@progos.org","customer Subscription",$messagenew,$headers) ){ ?>
-      	 <script>
-      	 window.location.href = "http://elephantationlabs.com/Whereismycreo/Whereismycreo/index.php?sucess=1";
-      	 </script>
+         <script>
+         window.location.href = "http://elephantationlabs.com/Whereismycreo/Whereismycreo/index.php?sucess=1";
+         </script>
       <?php }
    endif;
   }catch (Exception $e) {
