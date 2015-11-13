b359025cad7f008cc984c4eb28b0c3e3af878dd0
unknown <chkashif167@hotmail.com>
2015-11-13 09:02:10 +0500

    done

--- js-kit/createpaymenttoken.php
+++ js-kit/createpaymenttoken.php
@@ -44,7 +44,7 @@ foreach($products as $id => $product){
 $tokenPayload->setCurrency("AED");
 $tokenPayload->setAutoCapture("Y");
 $tokenPayload->setAutoCapTime(0.0);
-$tokenPayload->setValue("100");
+$tokenPayload->setValue($totalpricenew);
 $tokenPayload->setCustomerIp("88.216.3.135");
 $tokenPayload->setDescription("test");
 $tokenPayload->setEmail("test@test.com");
